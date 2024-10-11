9dd22985864bb01f9b2c5dd61d6ecc3bf09aca8a
ChoirulU <139779742+ChoirulU@users.noreply.github.com>
2024-10-11 14:58:10 +0700

    Pertemuan 4

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,34 @@
-// // Import global styles
-// import "./styles/global.css";
-// // Import CSS module
-// import styles from "./styles/global.module.css";
+"use client"; // Tambahkan ini di bagian paling atas
 
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.navbar}>Navbar</div>
-//       <div className={styles.sidebar}>Sidebar
-//         <ul><li><a href="/homepage"></a>Home Page</li>
-//             <li>Um cel</li>
-//             <li>Um cel</li>
-//             <li>Um cel</li>
-//         </ul>
-//       </div>
-//       <div className={styles.konten}>Konten</div>
-//       <div className={styles.footer}>Footer</div>
-//     </div>
-//   );
-// }
-// Import global styles
+import { useState } from 'react';
 import "./styles/global.css";
-// Import CSS module
 import styles from "./styles/global.module.css";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.navbar}>Navbar</div>
+      <div className={styles.navbar}>
+        <span className={styles.brand}>Mandala Budaya</span>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          &#9776; {/* Ikon garis 3 (Hamburger) */}
+        </div>
+        <ul className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ''}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li className={styles.closeButton} onClick={closeMenu}>&times;</li> {/* Tombol close */}
+        </ul>
+      </div>
+
       <div className={styles.sidebar}>
         Sidebar
         <ul>
@@ -37,6 +38,7 @@ export default function Home() {
           <li>Wisata Banyuwangi</li>
         </ul>
       </div>
+
       <div className={styles.konten}>Konten</div>
       <div className={styles.footer}>Footer</div>
     </div>
